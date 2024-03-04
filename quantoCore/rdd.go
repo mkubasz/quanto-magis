aa8ea828677379879dfb8bf87ab1e63f064abbdc
Mateusz Kubaszek <mkubasz@gmail.com>
2024-03-04 21:00:04 +0100

    feat: add for rdd map and filter

--- quantoCore/rdd.go
+++ quantoCore/rdd.go
@@ -15,3 +15,21 @@ func RDDCreateFromArray[T any](data []T) *RDD[T] {
 func (s *QuantoSession) Parallelize(data []interface{}) *RDD[interface{}] {
 	return RDDCreateFromArray(data)
 }
+
+func (r *RDD[T]) Map(f func(T) T) *RDD[T] {
+	var newData []T
+	for _, d := range r.data {
+		newData = append(newData, f(d))
+	}
+	return RDDCreateFromArray(newData)
+}
+
+func (r *RDD[T]) Filter(f func(T) bool) *RDD[T] {
+	var newData []T
+	for _, d := range r.data {
+		if f(d) {
+			newData = append(newData, d)
+		}
+	}
+	return RDDCreateFromArray(newData)
+}
--- quantoCore/rdd_test.go
+++ quantoCore/rdd_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -13,5 +14,49 @@ func TestRDDCreateFromArray(t *testing.T) {
 	}
 	if rdd.size != 5 {
 		t.Errorf("RDD size is not 5")
+	}
+}
+
+func TestRDDCreateFromStringArray(t *testing.T) {
+	session := NewQuantoSession()
+	data := []interface{}{"A", "B", "C", "D", "E"}
+	rdd := session.Parallelize(data)
+	if rdd == nil {
+		t.Errorf("RDD is nil")
+	}
+	if rdd.size != 5 {
+		t.Errorf("RDD size is not 5")
+	}
+}
+
+func lowerCase(s interface{}) interface{} {
+	return strings.ToLower(s.(string))
+}
+
+func TestRDDShouldLowerCase(t *testing.T) {
+	session := NewQuantoSession()
+	data := []interface{}{"A", "B", "C", "D", "E"}
+	rdd := session.Parallelize(data)
+	result := rdd.Map(lowerCase)
+	if result == nil {
+		t.Errorf("result is nil")
+	}
+	if result.data[0] != "a" {
+		t.Errorf("result[0] is not a")
+	}
+}
+
+func TestRDDShouldFilterCLetter(t *testing.T) {
+	session := NewQuantoSession()
+	data := []interface{}{"A", "B", "C", "D", "E"}
+	rdd := session.Parallelize(data)
+	result := rdd.Filter(func(s interface{}) bool {
+		return s.(string) != "C"
+	})
+	if result == nil {
+		t.Errorf("result is nil")
+	}
+	if result.size != 4 {
+		t.Errorf("result size is not 4")
 	}
 }
