91b9d81de4d5d25a3518ef39eabb2a968030425f
Mateusz Kubaszek <mkubasz@gmail.com>
2024-03-12 20:41:57 +0100

    feat: implement distinct and Select

--- quantoCore/dataframe.go
+++ quantoCore/dataframe.go
@@ -1,13 +1,15 @@
 package main
 
+import "errors"
+
 type Series[T any] struct {
 	data []T
 }
 
 type DataFrame struct {
-	series []Series[interface{}]
+	series  []Series[interface{}]
 	columns []string
-	size   int
+	size    int
 }
 
 func (s *RDD[T]) ToDF() *DataFrame {
@@ -31,7 +33,38 @@ func NewDataFrame(data []interface{}, columns []string) *DataFrame {
 		series = append(series, serie)
 	}
 	return &DataFrame{
-		size:   size,
-		series: series,
+		size:    size,
+		series:  series,
+		columns: columns,
 	}
+}
+
+func (df *DataFrame) Select(columnName string) (Series[interface{}], error) {
+	idx, err := df.getColumnIndex(columnName)
+	if err != nil {
+		return Series[interface{}]{}, err
+	}
+	return df.series[idx], nil
+}
+
+func (df *DataFrame) getColumnIndex(name string) (int, error) {
+	for idx, column := range df.columns {
+		if column == name {
+			return idx, nil
+		}
+	}
+	return -1, errors.New("column not found")
+}
+
+func (s Series[T]) Distinct(key string) (Series[interface{}], error) {
+	uniqueValues := make(map[interface{}]struct{})
+	for _, serie := range s.data {
+		uniqueValues[serie] = struct{}{}
+	}
+
+	distinctValues := make([]interface{}, 0, len(uniqueValues))
+	for k := range uniqueValues {
+		distinctValues = append(distinctValues, k)
+	}
+	return Series[interface{}]{data: distinctValues}, nil
 }
--- quantoCore/dataframe_test.go
+++ quantoCore/dataframe_test.go
@@ -31,3 +31,26 @@ func TestShouldCreateBasicDataFrameWithTwoColumns(t *testing.T) {
 		t.Error("Failed to create DataFrame")
 	}
 }
+
+func TestShouldSelectSelectedColumn(t *testing.T) {
+	columnOne := []interface{}{"A", "B", "C", "D", "E"}
+	columnTwo := []interface{}{1, 2, 3, 4, 5}
+	df := NewDataFrame([]interface{}{columnOne, columnTwo}, []string{"col1", "col2"})
+	col1, err := df.Select("col1")
+	size := len(col1.data)
+	if size != 5 && err != nil {
+		t.Error("Failed creating column")
+	}
+}
+
+func TestShouldDistinctSelectedColumn(t *testing.T) {
+	columnOne := []interface{}{"A", "B", "A", "D", "E"}
+	columnTwo := []interface{}{1, 2, 3, 4, 5}
+	df := NewDataFrame([]interface{}{columnOne, columnTwo}, []string{"col1", "col2"})
+	col1, _ := df.Select("col1")
+	distincted, err := col1.Distinct("A")
+	size := len(distincted.data)
+	if size != 4 && err != nil {
+		t.Error("Failed creating column")
+	}
+}
