1f674e4ed2c89c49bd432e88a1e6f6dafa97abb2
Mateusz Kubaszek <mkubasz@gmail.com>
2024-03-14 22:39:54 +0100

    feat: add agg methods

--- quantoCore/dataframe_groupby.go
+++ quantoCore/dataframe_groupby.go
@@ -1,9 +1,27 @@
 package main
 
 type DataFrameGroupBy struct {
-	df *DataFrame
+	df         *DataFrame
+	columnName string
+	aggs       []func() *DataFrame
 }
 
 func (df *DataFrame) GroupBy(name string) *DataFrameGroupBy {
-	return &DataFrameGroupBy{df: df}
+	return &DataFrameGroupBy{
+		df:         df,
+		columnName: name,
+	}
+}
+
+func (dfg *DataFrameGroupBy) Agg(f func() *DataFrame) *DataFrameGroupBy {
+	dfg.aggs = append(dfg.aggs, f)
+	return dfg
+}
+
+func (dfg *DataFrameGroupBy) Show() *DataFrame {
+	return nil
+}
+
+func (df *DataFrame) Count() *DataFrame {
+	return nil
 }
