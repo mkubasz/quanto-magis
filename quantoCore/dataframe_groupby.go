267a0b6f5d606ca4449ef9c46b76c221372faabe
Mateusz Kubaszek <mkubasz@gmail.com>
2024-03-18 21:33:53 +0100

    Add GroupBy functionality to DataFrame class

--- quantoCore/dataframe_groupby.go
+++ quantoCore/dataframe_groupby.go
@@ -1,16 +1,39 @@
 package main
 
+import "errors"
+
 type DataFrameGroupBy struct {
 	df         *DataFrame
 	columnName string
 	aggs       []func() *DataFrame
+	groups     map[interface{}][]int
 }
 
-func (df *DataFrame) GroupBy(name string) *DataFrameGroupBy {
+func (df *DataFrame) GroupBy(name string) (*DataFrameGroupBy, error) {
+	if !df.HasColumn(name) {
+		return nil, errors.New("column not found")
+	}
+
+	// Create a group by object
+	index, _ := df.getColumnIndex(name)
+	groups := make(map[interface{}][]int)
+	for idx, el := range df.series[index].data {
+		groups[el] = append(groups[el], idx)
+	}
 	return &DataFrameGroupBy{
 		df:         df,
 		columnName: name,
+		groups:     groups,
+	}, nil
+}
+
+func (df *DataFrame) HasColumn(name string) bool {
+	for _, column := range df.columns {
+		if column == name {
+			return true
+		}
 	}
+	return false
 }
 
 func (dfg *DataFrameGroupBy) Agg(f func() *DataFrame) *DataFrameGroupBy {
@@ -19,6 +42,7 @@ func (dfg *DataFrameGroupBy) Agg(f func() *DataFrame) *DataFrameGroupBy {
 }
 
 func (dfg *DataFrameGroupBy) Show() *DataFrame {
+
 	return nil
 }
 
