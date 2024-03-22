3bb5f8e62bfdd64ab8bbd8857e25585ac93a1074
Mateusz Kubaszek <mkubasz@gmail.com>
2024-03-22 23:16:57 +0100

    feat: add cli

---
+++ quantoCore/quantoCli.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "Quanto",
+		Usage: "quanto manager for computing data",
+		Action: func(*cli.Context) error {
+			fmt.Println("boom! I say!")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
